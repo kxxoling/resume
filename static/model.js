720ca352eaf9c0f461cb4f79359ab3986d337bb1
kxxoling <kxxoling@gmail.com>
2015-01-24 09:22:11 +0800

    修改 model 结构、Angular 绑定

--- static/model.js
+++ static/model.js
@@ -1,15 +1,11 @@
 var app= angular.module('app', [])
 app.controller('resumeController', function ($scope) {
-    $scope.timeline = [{
+
+    workExp = [{
         startTime: '2014-07',
         company: '北京天使汇',
         title: '前后端开发工程师',
-        text: '',
-        content: {
-            'HTML&CSS': 40,
-            Javascript: 70,
-            Python: 80
-        }
+        text: '涉及项目几乎所有方面的开发；带过新人、做过演讲。总的来说，得到了相对全面的成长。',
     }, {
         startTime: '2014-06',
         stopTime: '2014-07',
@@ -29,7 +25,61 @@ app.controller('resumeController', function ($scope) {
             Editor: 80,
             Journalist: 60
         }
+    }, {
+        startTime: '2008-09',
+        stopTime: '2012-07',
+        school: '北京信息科技大学',
+        major: '信息管理与信息系统',
+        text: '一个涉及广泛的专业：学过 C/C++，做过 Java/.NET，学过经济学、管理学，也涉及到项目管理、研发、测试。',
+        content: {
+            Editor: 80,
+            Journalist: 60
+        }
     }];
+
+    projectExp = [{
+        title: '天使汇主站',
+        text: '',
+        content: {
+            'HTML&CSS': 40,
+            'Javascript': 70,
+            'Python': 80
+        }
+    }, {
+        title: '天使汇统一登录系统',
+        text: '',
+        content: {
+            'HTML&CSS': 40,
+            'Javascript': 70,
+            'Python': 80
+        }
+    }, {
+        title: '天使汇自动补全系统',
+        text: '',
+        content: {
+            'HTML&CSS': 40,
+            'Javascript': 70,
+            'Python': 80
+        }
+    }, {
+        title: '车辆追踪系统',
+        text: '',
+        content: {
+            'HTML&CSS': 40,
+            'Javascript': 70,
+            'Python': 80
+        }
+    }, {
+        title: '各语言（微）博客',
+        text: '分别使用 Java/C#/Node.js/Python 开发过博客系统',
+        content: {
+            'HTML&CSS': 40,
+            'Javascript': 70,
+            'Python': 80
+        }
+    }];
+
+    $scope.timeline = workExp;
 
     $scope.profile = {
         name: '王然',
@@ -40,11 +90,9 @@ app.controller('resumeController', function ($scope) {
         github: 'kxxoling',
         bitbucket: 'windrunner1992',
         zhihu: 'kxxoling',
-        v2ex: ''
+        v2ex: '',
+        mail: 'kxxoling@gmail.com'
     };
 
-    $scope.skillsets = [{
-        name: 'Python',
-        percent: 70
-    }]
+    $scope.skillsets = $scope.timeline[0].content;
 });
