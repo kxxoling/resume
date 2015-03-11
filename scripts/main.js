0c74fc9407a0a3d311e22c90c1fff3250546cfea
kxxoling <kxxoling@gmail.com>
2015-03-11 23:26:00 +0800

    fix Google+1 async error

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,21 @@
 (function($){
 
-	"use strict";
+    "use strict";
 
     window.onload = function() {
-        angular.bootstrap(document, ['app'])
+        angular.bootstrap(document, ['app']);
 
-        $("#loader-wrapper").removeClass("loading").addClass("loaded")
+        $("#loader-wrapper").removeClass("loading").addClass("loaded");
 
         $('.hi .detail').typeTo("我是王然，这是我的个人简历。页面正在施工中，你可以随便看看，也欢迎稍后再来！");
+
+        window.___gcfg = {lang: 'zh-CN'};
+
+        (function() {
+            var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+            po.src = 'https://apis.google.com/js/platform.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+        })();
     }
 
 })(jQuery);
